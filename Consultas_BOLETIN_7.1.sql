fbc051e49b04984f336968e5562c5ce405fcaff2
LuisVergaraCastro <48942067+LuisVergaraCastro@users.noreply.github.com>
2022-06-02 12:42:21 +0200

    Update Consultas_BOLETIN_7.1.sql

--- Consultas_BOLETIN_7.1.sql
+++ Consultas_BOLETIN_7.1.sql
@@ -26,20 +26,59 @@ SELECT * FROM Employees
 SELECT FirstName, LastName, ISNULL(City, 'Sevilla') AS [Ciudad], ISNULL(DATEDIFF(YEAR, BirthDate, CURRENT_TIMESTAMP), 0) AS [EDAD]
 FROM Employees
 ORDER BY YEAR(BirthDate) 
+GO
 --4. Nombre y precio de cada producto, ordenado de mayor a menor precio.
 SELECT * FROM Products
 
 SELECT ProductName, UnitPrice
 FROM Products
-GROUP BY
+GROUP BY ProductName, UnitPrice
+ORDER BY UnitPrice DESC
+GO
 --5. Nombre de la compañía y dirección completa de cada proveedor de algún país de
 --América del Norte.
+SELECT * FROM Suppliers
+
+SELECT CompanyName, Address--, Country
+FROM Suppliers
+WHERE Country LIKE 'USA'
+GO
 --6. Nombre del producto, número de unidades en stock y valor total del stock, de los
 --productos que no sean de la categoría 4.
+SELECT * FROM Products
+
+SELECT ProductName, COUNT(UnitsInStock) AS [NUMERO DE UNIDADES], SUM(UnitsInStock * UnitPrice) AS [VALOR TOTAL DEL STOCK]
+FROM Products
+GROUP BY ProductName
+GO
 --7. Clientes (Nombre de la Compañía, dirección completa, persona de contacto) que no
 --residan en un país de América del Norte y que la persona de contacto no sea el
 --propietario de la compañía
+SELECT * FROM Customers
+
+SELECT CompanyName, Address, ContactName
+FROM Customers
+WHERE Country NOT LIKE 'USA' AND ContactName <> 'Owner'
+GO
 --8. ID del cliente y número de pedidos realizados por cada cliente, ordenado de mayor a
 --menor número de pedidos.
+SELECT * FROM Orders 
+
+SELECT CustomerID, COUNT(*) AS [NUMERO DE PEDIDOS]
+FROM Orders
+GROUP BY CustomerID
+ORDER BY [NUMERO DE PEDIDOS] DESC
+GO
 --9. Número de pedidos enviados a cada ciudad, incluyendo el nombre de la ciudad.
+SELECT * FROM Orders
+
+SELECT ShipCity, COUNT(*) AS [NUMERO DE PEDIDOS]
+FROM Orders
+GROUP BY ShipCity
+GO
 --10. Número de productos de cada categoría. 
+SELECT * FROM Products
+
+SELECT CategoryID, COUNT(*) AS [NUMERO DE PROUCTOS]
+FROM Products
+GROUP BY CategoryID
