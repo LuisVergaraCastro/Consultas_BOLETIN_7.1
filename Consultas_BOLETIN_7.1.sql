6438cf74dc88e2f076539569c0d08fefc69193f4
LuisVergaraCastro <48942067+LuisVergaraCastro@users.noreply.github.com>
2022-06-02 10:48:28 +0200

    Update Consultas_BOLETIN_7.1.sql

--- Consultas_BOLETIN_7.1.sql
+++ Consultas_BOLETIN_7.1.sql
@@ -7,13 +7,31 @@ USE Northwind
 --Consultas sobre una sola Tabla
 --1. Nombre de la compañía y dirección completa (dirección, cuidad, país) de todos los
 --clientes que no sean de los Estados Unidos.
-SELECT *
-FROM 
+SELECT * FROM Customers
 
+SELECT CompanyName, Address, City, Country
+FROM Customers
+WHERE Country <> 'USA'
+GO
 --2. La consulta anterior ordenada por país y ciudad.
+SELECT CompanyName, Address, City, Country
+FROM Customers
+WHERE Country <> 'USA'
+ORDER BY Country, City
+GO
 --3. Nombre, Apellidos, Ciudad y Edad de todos los empleados, ordenados por antigüedad en
 --la empresa.
+SELECT * FROM Employees
+
+SELECT FirstName, LastName, ISNULL(City, 'Sevilla') AS [Ciudad], ISNULL(DATEDIFF(YEAR, BirthDate, CURRENT_TIMESTAMP), 0) AS [EDAD]
+FROM Employees
+ORDER BY YEAR(BirthDate) 
 --4. Nombre y precio de cada producto, ordenado de mayor a menor precio.
+SELECT * FROM Products
+
+SELECT ProductName, UnitPrice
+FROM Products
+GROUP BY
 --5. Nombre de la compañía y dirección completa de cada proveedor de algún país de
 --América del Norte.
 --6. Nombre del producto, número de unidades en stock y valor total del stock, de los
